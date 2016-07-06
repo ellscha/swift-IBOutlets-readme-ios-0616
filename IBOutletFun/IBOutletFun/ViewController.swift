593f9a373f4db06a87f26676ed6c334dff398c6e
ellscha <escharlin@wesleyan.edu>
2016-07-06 16:38:57 -0400

    goodbye, project

--- IBOutletFun/IBOutletFun/ViewController.swift
+++ IBOutletFun/IBOutletFun/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textDisplay: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.textDisplay.text = "Zzzz"
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBAction func goodbyeWorldTapped(sender: AnyObject) {
 
+        self.textDisplay.text = "see ya later, world!"
+
+        print("pressed hello world button")
+    }
+
+    @IBAction func helloWorldTapped(sender: AnyObject) {
+
+        self.textDisplay.text = "hello, world!"
+
+        print("pressed goodbye button")
+    }
+    @IBAction func unicornTapped(sender: AnyObject) {
+
+        self.textDisplay.text = "🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄"
+        print("pressed unicorn!, \(self.textDisplay.font)")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
